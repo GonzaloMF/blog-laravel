70b7514dd6f8a6383dfa1315aadeab749f24f1a3
Gonzalo <gmandrionflores@gmail.com>
2023-01-06 00:25:14 +0000

    Backend development, views separetly coded. Fortify implemented.

--- resources/views/theme/back/app.blade.php
+++ resources/views/theme/back/app.blade.php
@@ -14,18 +14,20 @@
       content="Matrix Admin Lite Free Version is powerful and clean admin dashboard template, inpired from Bootstrap Framework"
     />
     <meta name="robots" content="noindex,nofollow" />
-    <title>@yield("title") - Blog CS348</title>
+    <title>@yield("title","Title") - Blog CS348</title>
     <!-- Favicon icon -->
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="{{asset(assets/back/images/favicon.png)}}"
+      href="{{asset("assets/back/images/favicon.png")}}"
     />
+
     <!-- Custom CSS -->
-    <link href="../assets/libs/flot/css/float-chart.css" rel="stylesheet" />
-    <!-- Custom CSS -->
-    <link href="{{asset(assets/back/css/style.min.css)}}" rel="stylesheet" />
+    <link href="{{asset("assets/back/css/style.min.css")}}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -35,10 +37,56 @@
   </head>
 
   <body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+          <div class="lds-pos"></div>
+          <div class="lds-pos"></div>
+        </div>
+      </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <div id="main-wrapper">
+    <!-- ============================================================== -->
+    <!-- Topbar header - style you can find in pages.scss -->
+    @include("theme.back.topheader")
+    <!-- End Topbar header -->
+    <!-- ============================================================== -->
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    @include("theme.back.aside")
+    <!-- ============================================================== -->
+    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <div class="page-wrapper">
+            <!-- Container fluid  -->
+            <div class="container-fluid">
 
 
+            </div>
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            @include("theme.back.footer")
+            <!-- End footer -->
+        </div>
+    </div>
 
-
-
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{asset('assets/back/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/back/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js')}}"></script>
+    <script src="{{asset('assets/back/libs/sparkline/sparkline.js')}}"></script>
+    <!--Wave Effects -->
+    <script src="{{asset('assets/back/js/waves.js')}}"></script>
+    <!--Menu sidebar -->
+    <script src="{{asset('assets/back/js/sidebarmenu.js')}}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{asset('assets/back/js/custom.min.js')}}"></script>
+    <!--This page JavaScript -->
+    <!-- <script src="{{asset("assets/back/js/pages/dashboards/dashboard1.js")}}"></script> -->
   </body>
+</html>
---
+++ resources/views/theme/back/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer text-center">
+    All Rights Reserved. Designed and Developed by
+    <a href="https://www.wrappixel.com">WrapPixel</a> and partly modified by Gonzalo M Flores (2026765).
+</footer>
